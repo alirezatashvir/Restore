1f5b1f3916315c9a8c19bfbdbfcb52936c2359ba
Alireza Tashvir <alireza.tashvir@solidbeans.com>
2025-12-15 15:22:44 +0100

    Add toggling between dark and light mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react"
 import { type Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container } from "@mui/material";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import NavBar from "./Navbar";
 
 
 function App() {
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const paletteType = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: (paletteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode)
+  }
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -16,12 +32,19 @@ function App() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Container maxWidth='xl' sx={{mt: 14}}>
-        <Catalog products={products} />
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: darkMode ? '#121212' : '#eaeaea'
+        }}>
+          <Container maxWidth='xl' sx={{mt: 14}}>
+            <Catalog products={products} />
+          </Container>
+      </Box>
+    </ThemeProvider>
   )
 }
 
